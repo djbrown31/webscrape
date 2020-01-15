f5d0aca625e42600ac3677d359222ab85b4d52c0
Dan Brown <brown.danny.j@gmail.com>
2020-01-15 10:59:59 -0600

    another commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('new_search', views.new_search, name='new_search'),
+    path('new_search', views.new_search, name='new_search'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+# from requests.compat import quote_plus
+from . import models
+
+# BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+# BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
 
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    stuff_for_frontend = { 
+        'search': search 
+        }
+    return render(request, 'myapp/new_search.html', stuff_for_frontend)
